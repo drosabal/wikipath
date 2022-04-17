02f364d4534074a5986e201cf281eaf8265dc948
dkirkendall1 <87043544+dkirkendall1@users.noreply.github.com>
2022-04-16 21:48:59 -0400

    Update Graph.cpp

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,50 +1,51 @@
 #include "Graph.h"
 #include <fstream>
 #include <sstream>
+using namespace std;
 
 Graph::Graph()
 {
-    std::ifstream file("wikilinks.txt");
-    std::string line;
+    ifstream file("wikilinks.txt");
+    string line;
     int i = 1;
-     while (std::getline(file, line)) {
-        std::istringstream line_stream(line);
-        std::string page_title;
-        std::getline(line_stream, page_title, '|');
+     while (getline(file, line)) {
+        istringstream line_stream(line);
+        string page_title;
+        getline(line_stream, page_title, '|');
         page_id.emplace(page_title, i);
         i++;
     }
     file.close();
 }
 
-bool Graph::page_exists(std::string page_title)
+bool Graph::page_exists(string page_title)
 {
     return (page_id.find(page_title) != page_id.end());
 }
-void Graph::insert(std::string from, std::string to)
+void Graph::insert(string from, string to)
 {
     adj_list[from].push_back(to);
 }
-std::vector<std::string> Graph::find_adjacent(std::string page_title)
+vector<string> Graph::find_adjacent(string page_title)
 {
-    std::vector<std::string> adjacent;
-    std::string line;
-    std::ifstream file("wikilinks.txt");
+    vector<string> adjacent;
+    string line;
+    ifstream file("wikilinks.txt");
     for (int i = 0; i < page_id[page_title]; i++) {
-        std::getline(file, line);
+        getline(file, line);
     }
     file.close();
-    std::istringstream line_stream(line);
-    std::string token;
-    std::getline(line_stream, token, '|');
-    while (std::getline(line_stream, token, '|')) {
+    istringstream line_stream(line);
+    string token;
+    getline(line_stream, token, '|');
+    while (getline(line_stream, token, '|')) {
         adjacent.push_back(token);
     }
     return adjacent;
 }
-std::vector<std::vector<std::string>> Graph::find_paths(std::string from, std::string to)
+vector<vector<string>> Graph::find_paths(string from, string to)
 {
-    std::vector<std::vector<std::string>> paths;
+    vector<vector<string>> paths;
     // TODO: Implement
     return paths;
 }

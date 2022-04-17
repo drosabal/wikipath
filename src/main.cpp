71b13a4f3183b8ab15ef2d2cbddfd173d5123bce
dkirkendall1 <87043544+dkirkendall1@users.noreply.github.com>
2022-04-16 21:45:52 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include "Graph.h"
+using namespace std;
 
 int main()
 {
     Graph graph;
 
-    std::string from, to;
-    std::cout << "Start: ";
-    std::cin >> from;
-    std::cout << "End: ";
-    std::cin >> to;
-    std::cout << std::endl;
+    string from, to;
+    cout << "Start: ";
+    cin >> from;
+    cout << "End: ";
+    cin >> to;
+    cout << endl;
 
     bool valid_search = true;
     if (!graph.page_exists(from)) {
-        std::cout << "Invalid start page title." << std::endl;
+        cout << "Invalid start page title." << endl;
         valid_search = false;
     }
     if (!graph.page_exists(to)) {
-        std::cout << "Invalid end page title." << std::endl;
+        cout << "Invalid end page title." << endl;
         valid_search = false;
     }
     if (valid_search) {
-        std::vector<std::vector<std::string>> paths = graph.find_paths(from, to);
+        vector<vector<string>> paths = graph.find_paths(from, to);
         // TODO: Output found paths
     }
 

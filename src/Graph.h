7a3807b7cdaabc442e508a861dc0f184a7af56bc
drosabale <103462682+drosabale@users.noreply.github.com>
2022-04-20 22:30:12 -0400

    Update Graph.h

--- src/Graph.h
+++ src/Graph.h
@@ -6,16 +6,15 @@ using namespace std;
 
 class Graph
 {
-    unordered_map<string, vector<string>> adj_list; // Adjacency list to represent graph
     unordered_map<string, int> page_id; // Maps each page title to a unique page ID
-    unordered_map<string, string> parent;
+    unordered_map<int, int> source; // Tracks the source of each page when searching
 
 public:
     Graph(); // Generates the page_id map from wikilinks.txt, where each ID is the page's line number
 
     bool page_exists(string page_title); // Returns true if the page appears in the page_id map
-    void insert(string from, string to); // Adds an edge to the graph (from-to)
-    vector<string> find_adjacent(string page_title); // Returns the pages adjacent to the given page from wikilinks.txt
-    vector<string> bfs(string from, string to); //helper function that runs a breadth-first search on the information passed to it
-    vector<string> backtrace(string from, string to);
+    string get_page_title(int id); // Returns the page title of the given id
+    vector<int> find_adjacent(int id); // Returns the pages adjacent to the given page from wikilinks.txt
+    vector<string> bfs(string from, string to); // Runs a breadth-first search and returns the shortest path
+    vector<string> backtrace(string from, string to); // Helper function for bfs to trace the found path
 };
